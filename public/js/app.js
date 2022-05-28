32ad8e8fbafb806d6895fad9d7c4da13472ecbca
Sahubadi <mdsahuquillo@gmail.com>
2022-05-28 19:19:47 +0200

    Remmove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 
 
-console.log('Client side javascript file is loaded!')
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
